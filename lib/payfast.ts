367321b813de56790168ada9c5602e59f20c72e3
Kyle <kyle.corbett@chaser.io.com>
2025-12-20 13:18:34 +0200

    changed to true

--- lib/payfast.ts
+++ lib/payfast.ts
@@ -105,7 +105,7 @@ export function verifyPayFastSignature(
  * Get PayFast configuration from environment
  */
 export function getPayFastConfig() {
-  const isSandbox = process.env.PAYFAST_SANDBOX || false;
+  const isSandbox = process.env.PAYFAST_SANDBOX || true;
   
   return {
     merchantId: process.env.PAYFAST_MERCHANT_ID || '',
